4d4d9ec54b88f42c62f3b7c28f0a1abc302fc15a
naoto120424 <naotonaka0424@gmail.com>
2024-01-27 18:40:15 +0900

    Model LSTM update

--- model/lstm/lstm.py
+++ model/lstm/lstm.py
@@ -8,6 +8,7 @@ class LSTMClassifier(nn.Module):
         self.input_dim = cfg.NUM_ALL_FEATURES
         self.spec_dim = cfg.NUM_CONTROL_FEATURES
         self.output_dim = cfg.NUM_PRED_FEATURES
+        self.out_len = args.out_len
         self.num_hidden_units = args.d_model
         self.num_layers = args.e_layers
         self.dropout = args.dropout
@@ -20,14 +21,25 @@ class LSTMClassifier(nn.Module):
             batch_first=True,
         )
         self.spec_dense = nn.Linear(self.spec_dim, self.num_hidden_units)
-        self.predictor = nn.Sequential(
-            nn.Linear(self.num_hidden_units * 2, self.num_hidden_units),
+        self.linear = nn.Sequential(
+            nn.Linear(self.num_hidden_units, self.num_hidden_units),
             nn.ReLU(inplace=True),
-            nn.Linear(self.num_hidden_units, self.output_dim),
         )
+        
+        self.generators = nn.ModuleList([])
+        for _ in range(self.out_len):
+            self.generators.append(
+                nn.Sequential(nn.LayerNorm(self.num_hidden_units), nn.Linear(self.num_hidden_units, self.output_dim))
+            )
 
     def forward(self, x, spec, h=None):
         hidden1, _ = self.lstm(x, h)
         hidden2 = self.spec_dense(spec)
-        y = self.predictor(torch.cat([hidden1[:, -1, :], hidden2], dim=1))  # 最後のセルだけを取り出している
-        return y, _
+        y = self.linear(torch.cat([hidden1, hidden2], dim=1))
+        y = y.mean(dim=1)
+        
+        y_final = torch.zeros(x.shape[0], self.out_len, self.output_dim).to(x.device)
+        for i, generator in enumerate(self.generators):
+            y_final[:, i] = generator(y)
+            
+        return y_final, _
