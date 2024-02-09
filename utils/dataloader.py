2ce4d301857c59f0c1a86a100518dbea61224d63
naoto120424 <naotonaka0424@gmail.com>
2024-02-09 16:13:55 +0900

    delete timedata

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -39,7 +39,6 @@ def load_data(cfg, dataset, in_len=20, out_len=20, debug=False):
     Xdata = []
     Specdata = []
     Ydata = []
-    TimeData = []
     GTData = []
 
     for file in tqdm(csv_files):
@@ -51,7 +50,6 @@ def load_data(cfg, dataset, in_len=20, out_len=20, debug=False):
         input_time_list = []
         spec_list = []
         gt_list = []
-        t_list = []
 
         if dataset == "refrig_only" and single_data.shape[0] != 3299:
             del_files.append(file)
@@ -59,12 +57,10 @@ def load_data(cfg, dataset, in_len=20, out_len=20, debug=False):
             for t in range(single_data.shape[0] - in_len - out_len + 1):
                 input_time_list.append(single_data[t : t + in_len])
                 spec_list.append(spec_data[t + in_len: t + in_len + out_len])
-                gt_list.append(output_data[t + in_len: t + in_len + out_len])
-                t_list.append(np.arange(t+in_len+1, t+in_len+1+out_len, dtype=float))
+                gt_list.append(output_data[t + in_len : t + in_len + out_len])
             Xdata.append(np.array(input_time_list))
             Specdata.append(np.array(spec_list))
             Ydata.append(np.array(gt_list))
-            TimeData.append(np.array(t_list))
             GTData.append(single_data[:, -cfg.NUM_PRED_FEATURES:])
             # print(np.array(input_time_list).shape)
             # print(np.array(spec_list).shape)
@@ -73,13 +69,11 @@ def load_data(cfg, dataset, in_len=20, out_len=20, debug=False):
 
     data["inp"] = np.array(Xdata)           # shape(case_num, 3299-(in_len+out_len), in_len, num_all_features)
     data["spec"] = np.array(Specdata)       # shape(case_num, 3299-(in_len+out_len), out_len, num_control_features)
-    data["gt"] = np.array(Ydata)            # shape(case_num, 3299-(in_len+out_len), out_len, num_pred_features)
-    data["timedata"] = np.array(TimeData)   # shape(case_num, 3299-(in_len+out_len), out_len)
+    data["gt"] = np.array(Ydata)  # shape(case_num, 3299-(in_len+out_len), out_len, num_pred_features)
     data["gt_data"] = np.array(GTData)      # shape(case_num, 3299, num_all_features)
     # print('data["inp"]', data["inp"].shape)
     # print('data["spec"]', data["spec"].shape)
     # print('data["gt"]', data["gt"].shape)
-    # print('data["timedata"]', data['timedata'].shape)
     # print('data["gt_data"]', data["gt_data"].shape)
 
     data["feature_name"] = list(map(lambda x: x.split(".")[0], pd.read_csv(os.path.join(cfg.DATA_PATH, csv_files[0]), skiprows=0, dtype=str).columns))
@@ -137,7 +131,6 @@ class MazdaDataset(Dataset):
             "inp": torch.Tensor(self.data["inp"][index]),
             "spec": torch.Tensor(self.data["spec"][index]),
             "gt": torch.Tensor(self.data["gt"][index]),
-            "timedata" : torch.Tensor(self.data["timedata"][index]),
         }
 
 
@@ -148,18 +141,15 @@ def create_dataset(cfg, dataset, original_data, index_list, csv_files, is_train,
     input_data_list = []
     spec_data_list = []
     gt_data_list = []
-    time_data_list = []
 
     for index in index_list:
         input_data_list.append(original_data["inp"][index])
         spec_data_list.append(original_data["spec"][index])
         gt_data_list.append(original_data["gt"][index])
-        time_data_list.append(original_data["timedata"][index])
     input_array = np.array(input_data_list)
     spec_array = np.array(spec_data_list)
     gt_array = np.array(gt_data_list)
-    time_data_array = np.array(time_data_list)
-    # print(input_array.shape, spec_array.shape, gt_array.shape, time_data_array.shape)
+    # print(input_array.shape, spec_array.shape, gt_array.shape)
 
     mean_list, std_list = find_meanstd(cfg, dataset, index_list, csv_files) if is_train else (mean_list, std_list)
     print("\n\nTrain Dataset Normalization") if is_train else print("\n\nValidation Dataset Normalization")
@@ -180,14 +170,9 @@ def create_dataset(cfg, dataset, original_data, index_list, csv_files, is_train,
     for i in tqdm(range(gt_array.shape[3])):
         gt_array[:, :, :, i] = (gt_array[:, :, :, i] - mean_list[i+num_control_features+1]) / std_list[i+num_control_features+1]
 
-    print("\n[time data]")
-    for i in tqdm(range(time_data_array.shape[2])):
-        time_data_array[:, :, i] = (time_data_array[:, :, i] - mean_list[0]) / std_list[0]
-
     data["inp"] = rearrange(input_array, "a b c d -> (a b) c d")
     data["spec"] = rearrange(spec_array, "a b c d -> (a b) c d")
     data["gt"] = rearrange(gt_array, "a b c d -> (a b) c d")
-    data["timedata"] = rearrange(time_data_array, "a b c -> (a b) c")
 
     print("----------------------------------------------")
 
