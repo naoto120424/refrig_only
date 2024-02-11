000d202fa5f8a2ac7f60def5e2c1c67ad74fef18
naoto120424 <naotonaka0424@gmail.com>
2024-02-12 01:45:53 +0900

    update DeepOLSTM model

--- model/DeepOTransformer/deepolstm.py
+++ model/DeepOTransformer/deepolstm.py
@@ -28,58 +28,58 @@ class DeepOLSTM(nn.Module):
         )
         self.spec_dense = nn.Linear(cfg.NUM_CONTROL_FEATURES, args.d_model)
 
-        self.branch_before = nn.Linear((self.out_len + 1) * args.d_model, args.d_model)
         self.branch = nn.ModuleDict()
-        self.branch["LinM1"] = nn.Linear((self.out_len + 1) * args.d_model, self.out_len * self.num_pred_features * self.width)
-        self.branch["NonM1"] = nn.ReLU()
-        for i in range(2, self.branch_layers):
-            self.branch["LinM{}".format(i)] = nn.Linear(self.out_len * self.num_pred_features * self.width, self.out_len * self.num_pred_features * self.width)
-            self.branch["NonM{}".format(i)] = nn.ReLU()
-        self.branch["LinMout"] = nn.Linear(self.width, self.width)
+        if self.branch_layers == 1:
+            self.branch["LinM1"] = nn.Linear(args.d_model * 2, self.num_pred_features * self.width)
+        else:
+            self.branch["LinM1"] = nn.Linear(args.d_model * 2, self.num_pred_features * self.width)
+            self.branch["NonM1"] = nn.ReLU()
+            for i in range(2, self.branch_layers):
+                self.branch["LinM{}".format(i)] = nn.Linear(self.num_pred_features * self.width, self.num_pred_features * self.width)
+                self.branch["NonM{}".format(i)] = nn.ReLU()
+            self.branch["LinMout"] = nn.Linear(self.num_pred_features * self.width, self.num_pred_features * self.width)
 
         self.trunk = nn.ModuleDict()
-        for n in range(1, self.out_len + 1):
-            self.trunk["L{}_LinM1".format(n)] = nn.Linear(n * self.num_all_features, n * self.num_pred_features * self.width)
-            self.trunk["L{}_NonM1".format(n)] = nn.ReLU()
-            for i in range(2, self.trunk_layers + 1):
-                self.trunk["L{}_LinM{}".format(n, i)] = nn.Linear(n * self.num_pred_features * self.width, n * self.num_pred_features * self.width)
-                self.trunk["L{}_NonM{}".format(n, i)] = nn.ReLU()
+        self.trunk["LinM1"] = nn.Linear(self.num_all_features, self.num_pred_features * self.width)
+        self.trunk["NonM1"] = nn.ReLU()
+        for i in range(2, self.trunk_layers + 1):
+            self.trunk["LinM{}".format(i)] = nn.Linear(self.num_pred_features * self.width, self.num_pred_features * self.width)
+            self.trunk["NonM{}".format(i)] = nn.ReLU()
 
         self.params = nn.ParameterDict()
-        for i in range(1, self.out_len + 1):
-            self.params["L{}_bias".format(i)] = nn.Parameter(torch.zeros([i, self.num_pred_features]))
+        self.params["bias"] = nn.Parameter(torch.zeros([self.num_pred_features]))
 
     def forward(self, inp, spec, h=None):
         hidden1, _ = self.lstm(inp, h)
         hidden2 = self.spec_dense(spec)
 
-        x = torch.cat([hidden1[:, -1:, :], hidden2], dim=1)
-        x = rearrange(x, "bs l d -> bs (l d)")
+        hidden1 = repeat(hidden1[:, -1:, :], "bs l d -> bs (repeat l) d", repeat=self.out_len)
+        x = torch.cat([hidden1, hidden2], dim=-1)
 
         if self.branch_layers == 1:
             LinM = self.branch["LinM1"]
             x = LinM(x)
-            x = rearrange(x, "bs (out_len n d) -> bs out_len n d", out_len=self.out_len, n=self.num_pred_features)
+            x = rearrange(x, "bs l (n d) -> bs l n d", n=self.num_pred_features)
         else:
             for i in range(1, self.branch_layers):
                 LinM = self.branch["LinM{}".format(i)]
                 NonM = self.branch["NonM{}".format(i)]
                 x = NonM(LinM(x))
-            x = rearrange(x, "bs (out_len n d) -> bs out_len n d", out_len=self.out_len, n=self.num_pred_features)
             x = self.branch["LinMout"](x)
+            x = rearrange(x, "bs l (n d) -> bs l n d", n=self.num_pred_features)
 
         y = inp[:, -1:, :]
 
         for t in range(1, self.out_len + 1):
             y_out = y
-            y_out = rearrange(y_out, "bs len n -> bs (len n)")
             for i in range(1, self.trunk_layers + 1):
-                LinM = self.trunk["L{}_LinM{}".format(t, i)]
-                NonM = self.trunk["L{}_NonM{}".format(t, i)]
+                LinM = self.trunk["LinM{}".format(i)]
+                NonM = self.trunk["NonM{}".format(i)]
                 y_out = NonM(LinM(y_out))
 
-            y_out = rearrange(y_out, "bs (out_len n d) -> bs out_len n d", out_len=t, n=self.num_pred_features)
-            y_out = torch.nansum(x[:, :t] * y_out, dim=-1, keepdim=False) + self.params["L{}_bias".format(t)]
+            y_out = rearrange(y_out, "bs l (n d) -> bs l n d", n=self.num_pred_features)
+
+            y_out = torch.nansum(x[:, :t] * y_out, dim=-1, keepdim=False) + self.params["bias"]
 
             y_out = torch.cat((spec[:, :t], y_out), dim=-1)
             y = torch.cat((y, y_out[:, -1:]), dim=1)
